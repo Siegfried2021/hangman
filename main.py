3ba0f498745607038b548adb22a61e1f3b1ac45f
Siegfried2021 <lecouvetm@gmail.com>
2024-06-17 22:02:28 +0200

    last update project hangman

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from utils.game import Hangman
 
 def main():
     hangman = Hangman()
-    while hangman.lives > 0 and hangman.well_played() == False:
-            hangman.play()
-            hangman.well_played()
-            hangman.game_over()
+    while hangman.lives > 0 and hangman.correclty_guessed_letters != list(hangman.word_to_find):
+        hangman.start_game(hangman.play, hangman.game_over, hangman.well_played)
 
 if __name__ == "__main__":
     main()
--- utils/game.py
+++ utils/game.py
@@ -2,14 +2,28 @@ import random
 
 class Hangman:
     def __init__(self, lives: int=5):
-        self.possible_words = ['becode', 'learning', 'mathematics', 'sessions', 'python', 'success', 'burger']
+        self.possible_words = ['becode', 'learning', 'mathematics', 'sessions', 'python', 'success', 'burger', 'software']
         self.word_to_find = random.choice(self.possible_words)
         self.lives = lives
-        self.correclty_guessed_letters = []
+        self.correclty_guessed_letters = ["_"] * len(self.word_to_find)
         self.wrongly_guessed_letters = []
         self.turn_count = 0
         self.error_count = 0
-        
+
+    def start_game(self, play, game_over, well_played):
+        play()
+        self.turn_count += 1
+        print(f"Number of turns: {self.turn_count}")
+        print(f"Current state of the game: {self.correclty_guessed_letters}")
+        print(f"Number of errors: {self.error_count}")
+        if len(self.wrongly_guessed_letters) > 0:
+            print(f"The following proposed letters are not in the secret word: {self.wrongly_guessed_letters}")
+        print(f"Number of lives left: {self.lives}")
+        if self.lives == 0:
+            game_over()
+        if self.correclty_guessed_letters == list(self.word_to_find):
+            well_played()
+
     def play(self):
         while True:
             letter_proposed = input("Which letter do you want to propose? ").lower()
@@ -19,32 +33,19 @@ class Hangman:
             if letter_proposed in self.correclty_guessed_letters or letter_proposed in self.wrongly_guessed_letters:
                 print("You already proposed this letter!")
                 continue
-            self.turn_count += 1
-            self.turn_count
             if letter_proposed in self.word_to_find:
-                self.correclty_guessed_letters.append(letter_proposed)
+                list_word = list(self.word_to_find)
+                for i in range(len(self.word_to_find)):
+                    if list_word[i] == letter_proposed:
+                        self.correclty_guessed_letters[i] = letter_proposed
             else:
                 self.wrongly_guessed_letters.append(letter_proposed)
                 self.error_count += 1
-            
-            return self.turn_count, self.error_count, self.wrongly_guessed_letters, self.correclty_guessed_letters
+                self.lives -= 1
+            return self.turn_count, self.error_count, self.lives, self.wrongly_guessed_letters, self.correclty_guessed_letters
             
     def well_played(self):
-        displayed_word = len(self.word_to_find) * "_"
-        list_displayed = list(displayed_word)
-        for letter in self.correclty_guessed_letters:
-            for i in range(len(self.word_to_find)):
-                if self.word_to_find[i] == letter:
-                    list_displayed[i] = letter
-                    displayed_word = "".join(list_displayed)
-        print(displayed_word)
-        if displayed_word == self.word_to_find:
-            print(f"You found the word: {self.word_to_find} in {self.turn_count} turns with {self.error_count} errors!")
-            return True
-        else:
-            return False
+        print(f"You found the word {self.word_to_find} in {self.turn_count} turns with {self.error_count} errors!")
         
     def game_over(self):
-        if self.error_count == self.lives:
-            self.lives = 0
-            print(f"Game over... The secret word was {self.word_to_find}.")
+        print(f"Game over... The secret word was {self.word_to_find}.")
