d01ab61466cd8455f21a58430667263f405f8dc3
Siegfried2021 <lecouvetm@gmail.com>
2024-06-17 10:44:26 +0200

    final version project

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-class Hangman:
-    def __init__(self)
-        self.possible_words = ['becode', 'learning', 'mathematics', 'sessions', 'science', 'machines']
-        self.word_to_find = 
+from utils.game import Hangman
+
+def main():
+    hangman = Hangman("becode")
+    if hangman.word_to_find in hangman.possible_words:
+        while hangman.lives > 0 and hangman.well_played() == False:
+            hangman.well_played()
+            hangman.game_over()
+            hangman.play()
+    else:
+        print("The target word is not accepted")
+
+if __name__ == "__main__":
+    main()
---
+++ utils/game.py
@@ -0,0 +1,47 @@
+class Hangman:
+    def __init__(self, word_to_find, lives: int=5):
+        self.possible_words = ['becode', 'learning', 'mathematics', 'sessions', 'python', 'success', 'burger']
+        self.word_to_find = word_to_find
+        self.lives = lives
+        self.correclty_guessed_letters = []
+        self.wrongly_guessed_letters = []
+        self.turn_count = 0
+        self.error_count = 0
+        
+    def play(self):
+        while True:
+            letter_proposed = input("Which letter do you want to propose? ").lower()
+            if len(letter_proposed) != 1:
+                print("You should propose only one letter!")
+                continue
+            if letter_proposed in self.correclty_guessed_letters or letter_proposed in self.wrongly_guessed_letters:
+                print("You already proposed this letter!")
+                continue
+            self.turn_count += 1
+            self.turn_count
+            if letter_proposed in self.word_to_find:
+                self.correclty_guessed_letters.append(letter_proposed)
+            else:
+                self.wrongly_guessed_letters.append(letter_proposed)
+                self.error_count += 1
+            
+            return self.turn_count, self.error_count, self.wrongly_guessed_letters, self.correclty_guessed_letters
+            
+    def well_played(self):
+        displayed_word = len(self.word_to_find) * "_"
+        list_displayed = list(displayed_word)
+        for letter in self.correclty_guessed_letters:
+            for i in range(len(self.word_to_find)):
+                if self.word_to_find[i] == letter:
+                    list_displayed[i] = letter
+                    displayed_word = "".join(list_displayed)
+        if displayed_word == self.word_to_find:
+            print(f"You found the word: {self.word_to_find} in {self.turn_count} turns with {self.error_count} errors!")
+            return True
+        else:
+            return False
+        
+    def game_over(self):
+        if self.error_count == self.lives:
+            self.lives = 0
+            print("Game over...")
